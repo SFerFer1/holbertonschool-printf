d7f5cbfac10823188185512133dea0c48cc4da75
SFerFer1 <10048@holbertonstudents.com>
2024-11-25 16:45:25 +0000

    esto tiene que imprimir si solo es texto sin variables

--- printf.c
+++ printf.c
@@ -1,15 +1,17 @@
 #include <stdarg.h>
+#include "main.h"
 int _printf(const char *format, ...)
 {
-char *-1str;
+char *-1str = "c";
+char *str;
 va_list words;
 va_start(words, format);
-char *str = va_arg(args, char*);
-char *-1str;
+char *str = va_arg(words, char*);
 
 
+acepta_char(*str , *-1str);
 
 
-char *str = va_arg(args, char*);
+char *-1str = va_arg(words, char*);
 va_end(words);
 }
