6648de34c2dd1953ae42764d90f9e21ad3bd7b1b
SFerFer1 <10048@holbertonstudents.com>
2024-11-25 13:57:11 +0000

    base

---
+++ printf.c
@@ -0,0 +1,15 @@
+#include <stdarg.h>
+int _printf(const char *format, ...)
+{
+char *-1str;
+va_list words;
+va_start(words, format);
+char *str = va_arg(args, char*);
+char *-1str;
+
+
+
+
+char *str = va_arg(args, char*);
+va_end(words);
+}
