31598061108b03d07b0743e9613ec2ae5bdf26bf
SFerFer1 <10048@holbertonstudents.com>
2024-12-01 16:33:29 +0000

    now all betty works

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <limits.h>
-
+/**
+ * main - Entry point for testing the _printf function.
+ *
+ * This function tests
+ *
+ * Return: Always 0 (Success).
+ */
 int main(void)
 {
- _printf(NULL);
+_printf(NULL);
 
- _printf("%d", INT_MIN);
- _printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
- _printf("%i", INT_MIN);
- _printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+_printf("%d", INT_MIN);
+_printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+_printf("%i", INT_MIN);
+_printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
 
 return (0);
 }
--- main.h
+++ main.h
@@ -1,12 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <limits.h>
-#ifndef MAIN_H
-#define MAIN_H
 int _printf(const char *format, ...);
-int recorrer_string(const char *str , va_list parametros);
+int recorrer_string(const char *strZZ, va_list parametros);
 int manejar_c_s(char definidor, va_list parametros, int *contador);
 int manejar_casos_i_d(va_list parametros, int *contador);
 int contar_digitos(int num);
